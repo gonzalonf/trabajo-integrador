709869283f50b1ea0745b22f9f20f5f6da2aebe5
Caro Avila <caroavilacaruso@gmail.com>
2017-04-28 11:29:40 -0300

    Arreglos en el mobile. Faltan desktop y tablet

--- html/registro.php
+++ html/registro.php
@@ -15,42 +15,36 @@
 	<!-- inicia el CONTENEDOR para el Registro -->
 
 	<div class="registro-container">
+			<div class="crear-cuenta">
+				<h1>CREAR CUENTA</h1>
+				<hr>
+			</div>
 			<form class="formulario" action="script.php" method="post">
-			<br>
-			<h1>CREAR CUENTA</h1>
-			<br>
-			<br>
-			<!-- Acá estaría bueno una línea que separe---->
-			<!-- gonza: ahí agregué...es el tag "<hr>" -->
-			<hr>
-			<br>
-			<br>
-			<input type="mail" name="mail" placeholder="Correo electrónico" class="decorative-input-mail" required id=mail>
-			<br><br>
-			<input type="text" name="nombre"  id=name placeholder="Nombre" class="decorative-input" required>
-			<br> <br>
-			<input type="text" name="last" id=last placeholder="Apellido" class="decorative-input" required>
-			<br> <br>
-			<input type="password" name="password" id=password placeholder="Contraseña" class="decorative-input-password" required >
-			<br><br>
-			<input type="password" name="password-confirmation" id=password-confirmation placeholder="Confirmar contraseña" class="decorative-input-password" require >
-			<br><br>
-			<div class="checkbox">
-			<input checked="checked" id="mail-promociones" name="mail-promociones" type="checkbox" value="1">
-			</div>
-			<label for="mail-promociones" class="me-gustaria">  Me gustaría recibir cupones, promociones, encuestas y actualizaciones por correo electrónico sobre Soy Mi Planner y sus socios.
-			</label>
-			<br>
-			<br>
-			<button type="submit" class="enviar" name="submit"><strong>REGISTRATE</strong></button>
-			<br>
-			<br>
-			<div class="aclaracion">
-			<p>Al registrarme, acepto las Condiciones del servicio, la Política de Privacidad y de Cookies.</p>
-			<br>
-			</div>
-		</form>
-<br><br><br>
+				<input type="mail" name="mail" placeholder="Correo electrónico" class="decorative-input-mail" required id=mail>
+				<br><br>
+				<input type="text" name="nombre"  id=name placeholder="Nombre" class="decorative-input" required>
+				<br> <br>
+				<input type="text" name="last" id=last placeholder="Apellido" class="decorative-input" required>
+				<br> <br>
+				<input type="password" name="password" id=password placeholder="Contraseña" class="decorative-input-password" required >
+				<br><br>
+				<input type="password" name="password-confirmation" id=password-confirmation placeholder="Confirmar contraseña" class="decorative-input-password" require >
+				<br><br>
+				<div class="checkbox">
+					<input checked="checked" id="mail-promociones" name="mail-promociones" type="checkbox" value="1">
+				</div>
+				<label for="mail-promociones" class="me-gustaria">  Me gustaría recibir cupones, promociones, encuestas y actualizaciones por correo electrónico sobre Soy Mi Planner y sus socios.
+				</label>
+				<br>
+				<br>
+				<button type="submit" class="enviar" name="submit"><strong>REGISTRATE</strong></button>
+				<br>
+				<br>
+				<div class="aclaracion">
+					<p>Al registrarme, acepto las Condiciones del servicio, la Política de Privacidad y de Cookies.</p>
+					<br>
+				</div>
+			</form>
 </div>
 
 <!-- login -->
