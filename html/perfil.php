9e3e0c3a16f06338bfbb2e21ce0d8867348927b3
Mijael Saban <mijaelsaban@gmail.com>
2017-05-03 12:07:51 -0300

    perfil

--- html/perfil.php
+++ html/perfil.php
@@ -14,17 +14,17 @@
           <img src="../images/avatar/default.png" alt="avatar">
       </div>
       <div class="info">
-          <?php if ( !isset($_SESSION) ) {
-              echo "Usted no ha sido correctamente logeado";
-          }exit;
-            ?>
+          <?php if ( !isset($_SESSION) || !count($_SESSION)): ?>
+               Usted no ha sido correctamente logeado"
+         <?php   else: ?>
+
           <ul>
               <li>Nombre de Usuario: <?php echo $_SESSION['userName']; ?></li>
               <li>Nombre:  <?php echo $_SESSION['nombre']; ?></li>
               <li>Apellido: <?php echo $_SESSION['apellido']; ?></li>
               <li>Email:  <?php echo $_SESSION['email']; ?></li>
           </ul>
-
+<?php endif; ?>
       </div>
       <?php include('footer.html'); ?>
 
