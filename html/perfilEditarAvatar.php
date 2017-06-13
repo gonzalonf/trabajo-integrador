0fee1382dae1da269b73577bbe9e43908422aa9c
Mir <mirnixx@gmail.com>
2017-06-13 19:59:15 -0300

    Editar Perfil Avatar funciona
    Editar Perfil Avatar funciona

--- html/perfilEditarAvatar.php
+++ html/perfilEditarAvatar.php
@@ -1,10 +1,18 @@
 <?php
 require("../php/soporte.php");
+require_once("../php/clases/validadorUsuario.php");
 
 $repoUsuarios = $repo->getRepositorioUsuarios();
-
 $usuarioLogueado = $auth->traerUsuarioLogueado($repoUsuarios);
 
+if (!$auth->estaLogueado()) {
+	header("Location:login.php");exit;
+}
+
+if (isset($_FILES['avatar'])) {
+	$usuarioLogueado->setAvatar($_FILES['avatar']);
+	header('Location: perfil.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -33,28 +41,25 @@ $usuarioLogueado = $auth->traerUsuarioLogueado($repoUsuarios);
 			<label for='avatar' class='text-label'>Imagen de perfil: </label> <br>
 			<input class='decorative-input-imagen-boton' type='file' name='avatar'> <br>
 
-			<p class='msj_error'><?php if (isset($errores["avatar"])) { 
+			<p class='msj_error'><?php if (isset($errores["avatar"])) {
 				echo $errores["avatar"];
-			} else {
-				//$usuarioLogueado->setNombre($_POST["nombre"]);
-				//$usuarioLogueado->guardar($repoUsuarios);
-			}
-			?></p>
-			<br>
+			}?>
+		</p>
+		<br>
 
-			<button type='submit' class='enviar' name='submit' value='registrate'><strong>CONFIRMAR</strong></button>
-			<br>
+		<button type='submit' class='enviar' name='submit' value='registrate'><strong>CONFIRMAR</strong></button>
+		<br>
 
-		</form>
+	</form>
+</div>
+
+<div class='registro-container' style="margin-top: -50px;">
+	<div class='formulario'>
+		<a class='volver' href="perfil.php">VOLVER</a>
 	</div>
+</div>
 
-	<div class='registro-container' style="margin-top: -50px;">
-		<div class='formulario'>
-			<a class='volver' href="perfil.php">VOLVER</a>
-		</div>
-	</div>
-
-	<?php include 'footer.html' ?>
+<?php include 'footer.html' ?>
 
 </body>
 </html>
